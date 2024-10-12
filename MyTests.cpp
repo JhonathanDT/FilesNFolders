6c4a801b52ec9e7b6db3236783853bbe0a557f89
David Tepan <davidt@Davids-World.lan>
2024-10-12 19:38:44 -0400

    the files class is perfection

--- MyTests.cpp
+++ MyTests.cpp
@@ -19,50 +19,54 @@ int main(){
     bptr[i] = i+6;
   }
   /* Testing names It WORKS */
-  // File a ("jpegmafia." , "i forgot my extension");
-  // std::cout << a << std::endl;
-  // File b ("jpegmafia" , "i forgot my period and my extension");
+  File b ("jpegmafia" , "i forgot my period and my extension");
   // std::cout << b << std::endl;
-  // // File d ("" , "");
-  // // std::cout << d << std::endl;
-  // File z ( ".txt" , "i got no name before my extension but that's ok");
-  // std::cout << z << std::endl;
-  // File p ("." , "i am only a period", bptr);
+  File d ("" , "");
+  std::cout << d << std::endl;
+  File z ( ".txt" , "i got no name before my extension but that's ok");
+  std::cout << z << std::endl;
+  File p ("." , "i am only a period");
   //  std::cout << p << std::endl;
-  // File p ("" , "i got no name", bptr);
-  //  std::cout << p << std::endl;
-  // File p ("Empty.contents" , "", bptr); //empty contents with a name
-  // std::cout << p << std::endl;
+  File g ("" , "i got no name", bptr);
+  File e ("Empty.contents" , ""); //empty contents with a name
 
   File c ("jpeg.txt" , "i am perfect", aptr); // used for the copy and move assignment
   /** Testing the Copy Constructor IT WORKS!!*/
-  /*
-  File c ("jpeg.txt" , "i am perfect", aptr);
-    std::cout << c << std::endl;
   File copiedFile = c;
   std::cout << "the name of the file is: " << copiedFile.getName() << " \nand the content: " << copiedFile.getContents() <<std::endl;
-  std::cout << "the name of the file is: " << c.getName() << " \nand the content: " << c.getContents() <<std::endl;
-  */
+  // std::cout << "the name of the file is: " << c.getName() << " \nand the content: " << c.getContents() <<std::endl;
+
   /* Testing the Move Constructor */
   File movedFile(std::move(c));
   std::cout << "the name of the moved file is: " << movedFile.getName() << " \nand the content: " << movedFile.getContents() <<std::endl;
-  std::cout << "the name of the file is: " << c.getName() << " \nand the content: " << c.getContents() <<std::endl;
+  // std::cout << "the name of the file is: " << c.getName() << " \nand the content: " << c.getContents() <<std::endl;
   
 
   /* Testing the Copy Assignment  IT WORKS!!*/
-  /*
+  //uses the moved file to work the copy assignment
   File copiedAssign ( "something.else", "not important it should contain copied");
-  copiedAssign = c;
+  copiedAssign = movedFile;
   std::cout << "the name of the file is: " << copiedAssign.getName() << " \nand the content: " << copiedAssign.getContents() <<std::endl;
-  std::cout << "the name of the file is: " << c.getName() << " \nand the content: " << c.getContents() <<std::endl;
-  */
-
-  /* Testing the Move Assignment */
+  // std::cout << "the name of the file is: " << c.getName() << " \nand the content: " << c.getContents() <<std::endl;
   
+
+  /* Testing the Move Assignment */ 
+  File a ("jpegmafia." , "i forgot my extension", bptr);
   File movedAssign ( "something.else", "not important it should be moved");
-  movedAssign = std::move(c);
+  movedAssign = std::move(a);
   std::cout << "the name of the movedAssign file is: " << movedAssign.getName() << " \nand the content: " << movedAssign.getContents() <<std::endl;
   std::cout << "the name of the file c is: " << c.getName() << " \nand the content: " << c.getContents() <<std::endl;
   
+
+  int* cptr = movedAssign.getIcon();
+  if( cptr == nullptr){
+    std::cout<< "the icon is empty" <<std::endl;
+  }
+  else{
+  //show the icon array
+  for(int i = 0; i < size; i++){
+    std::cout << cptr[i] << " ";
+  }
+  }
 
 }
